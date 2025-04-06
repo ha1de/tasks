5a13632df576d43818cf519043a20b68c9dd308b
ha1de <ha1den.boii@gmail.com>
2025-04-06 15:40:19 +0300

    VEIKIA ADD BOARD IR VISI LOGINAI

--- trackingg/Models/Issue.cs
+++ trackingg/Models/Issue.cs
@@ -1,10 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using trackingg.Data;
+﻿// --- START OF FILE ---
 
-namespace trackingg.Models
+// Required using statements
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation; // Needed for [ValidateNever]
+using trackingg.Data; // Your data namespace - adjust if needed
+
+namespace trackingg.Models // Your models namespace - adjust if needed
 {
-    // Existing Issue model with some enhancements
+    // --- Issue Class (Final Modifications) ---
     public class Issue
     {
         public uint Id { get; set; }
@@ -16,43 +22,52 @@ namespace trackingg.Models
         [Required]
         [StringLength(300)]
         public string Description { get; set; } = string.Empty;
-        public IssueType IssueType { get; set; }
-        public Priority Priority { get; set; }
+
+        public IssueType IssueType { get; set; } // Refers to IssueType enum defined elsewhere
+        public Priority Priority { get; set; } // Refers to Priority enum defined elsewhere
+
         public DateTime Created { get; set; }
         public DateTime? Completed { get; set; }
-        
-        // Foreign Keys
-        [Required]
+
+        // --- Foreign Keys ---
+
+        // [Required] // <-- MODIFIED: Removed [Required]
         public string AssignedToId { get; set; } = string.Empty;
-        public ApplicationUser AssignedTo { get; set; } = null!;
-        
-        public uint? ProjectId { get; set; }
-        public Project Project { get; set; } = null!;
-        
+
+        [ValidateNever] // <-- MODIFIED: Added [ValidateNever]
+        public ApplicationUser AssignedTo { get; set; } // <-- MODIFIED: Removed = null!;
+
+
+        public uint? ProjectId { get; set; } // Nullable Foreign Key
+
+        [ValidateNever] // <-- MODIFIED: Added [ValidateNever] (from previous step)
+        public Project Project { get; set; } // <-- MODIFIED: Removed = null!;
+
+
+        // --- Collections ---
         public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
         public virtual ICollection<Tag> Tags { get; set; } = new List<Tag>();
     }
 
+    // --- Other Model Classes (with null! removed from nav props) ---
     public class Project
     {
         public uint Id { get; set; }
-        
+
         [Required]
         [StringLength(100)]
         public string Name { get; set; } = string.Empty;
-        
+
         [StringLength(300)]
         public string Description { get; set; } = string.Empty;
-        
+
         public DateTime Created { get; set; }
         public DateTime? Completed { get; set; }
-        
-        // Foreign Keys
+
         [Required]
         public string OwnerId { get; set; } = string.Empty;
-        public ApplicationUser Owner { get; set; } = null!;
-        
-        // Navigation properties
+        public ApplicationUser Owner { get; set; } // <-- MODIFIED: Removed = null!;
+
         public virtual ICollection<Issue> Issues { get; set; } = new List<Issue>();
         public virtual ICollection<ProjectMember> Members { get; set; } = new List<ProjectMember>();
     }
@@ -60,74 +75,73 @@ namespace trackingg.Models
     public class ProjectMember
     {
         public uint Id { get; set; }
-        
-        // Foreign Keys
+
         public uint ProjectId { get; set; }
-        public Project Project { get; set; } = null!;
-        
+        public Project Project { get; set; } // <-- MODIFIED: Removed = null!;
+
         public string UserId { get; set; } = string.Empty;
-        public ApplicationUser User { get; set; } = null!;
-        
-        public ProjectRole Role { get; set; }
+        public ApplicationUser User { get; set; } // <-- MODIFIED: Removed = null!;
+
+        public ProjectRole Role { get; set; } // Refers to ProjectRole enum below
         public DateTime JoinedDate { get; set; }
     }
 
     public class Comment
     {
         public uint Id { get; set; }
-        
+
         [Required]
         [StringLength(1000)]
         public string Content { get; set; } = string.Empty;
-        
+
         public DateTime Created { get; set; }
         public DateTime? Updated { get; set; }
-        
-        // Foreign Keys
+
         public uint IssueId { get; set; }
-        public Issue Issue { get; set; } = null!;
-        
+        public Issue Issue { get; set; } // <-- MODIFIED: Removed = null!;
+
         [Required]
         public string AuthorId { get; set; } = string.Empty;
-        public ApplicationUser Author { get; set; } = null!;
+        public ApplicationUser Author { get; set; } // <-- MODIFIED: Removed = null!;
     }
 
     public class Tag
     {
         public uint Id { get; set; }
-        
+
         [Required]
         [StringLength(50)]
         public string Name { get; set; } = string.Empty;
-        
+
         [StringLength(7)]
-        public string Color { get; set; } = "#007bff"; // Default color
-        
+        public string Color { get; set; } = "#007bff";
+
         public virtual ICollection<Issue> Issues { get; set; } = new List<Issue>();
     }
 
     public class Activity
     {
         public uint Id { get; set; }
-        
+
         [Required]
         [StringLength(200)]
         public string Description { get; set; } = string.Empty;
-        
-        public ActivityType Type { get; set; }
+
+        public ActivityType Type { get; set; } // Refers to ActivityType enum below
         public DateTime Timestamp { get; set; }
-        
-        // Foreign Keys
+
         public string UserId { get; set; } = string.Empty;
-        public ApplicationUser User { get; set; } = null!;
-        
+        public ApplicationUser User { get; set; } // <-- MODIFIED: Removed = null!;
+
         public uint? IssueId { get; set; }
-        public Issue Issue { get; set; } = null!;
-        
+        public Issue Issue { get; set; } // <-- MODIFIED: Removed = null!;
+
         public uint? ProjectId { get; set; }
-        public Project Project { get; set; } = null!;
+        public Project Project { get; set; } // <-- MODIFIED: Removed = null!;
     }
 
+    // --- Enums used by the classes above ---
+    // --- Assuming IssueType and Priority are in separate files now ---
     public enum ProjectRole
     {
         Viewer,
@@ -147,4 +161,7 @@ namespace trackingg.Models
         MemberAdded,
         MemberRemoved
     }
-}
+
+} // --- End of namespace trackingg.Models ---
+
+// --- END OF FILE ---
